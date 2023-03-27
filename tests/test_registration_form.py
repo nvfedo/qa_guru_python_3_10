1a7c22a8a251efb4833e4ae013ce88392f86a6ff
Nikita Fedotov <nik197.1997@gmail.com>
2023-03-28 02:00:33 +0300

    first commit

---
+++ tests/test_registration_form.py
@@ -0,0 +1,57 @@
+import allure
+from utils import attachments
+from selene import have, by
+from selene.support.shared import browser
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+
+@allure.title("Successful fill form")
+def test_successful():
+    options = Options()
+    selenoid_capabilities = {
+        "browserName": "chrome",
+        "browserVersion": "111.0",
+        "selenoid:options": {
+            "enableVNC": True,
+            "enableVideo": False
+        }
+    }
+    options.capabilities.update(selenoid_capabilities)
+    driver = webdriver.Remote(
+        command_executor="https://{login}:{password}@selenoid.autotests.cloud/wd/hub",
+        options=options)
+
+    browser.config.driver = driver
+    first_name = "Nikita"
+    second_name = "Fedotov"
+
+    with allure.step("Open registrations form"):
+        browser.open("https://demoqa.com/automation-practice-form")
+        browser.element(".practice-form-wrapper").should(have.text("Student Registration Form"))
+        browser.driver.execute_script("$('footer').remove()")
+        browser.driver.execute_script("$('#fixedban').remove()")
+
+    with allure.step("Fill form"):
+        browser.element("#firstName").set_value(first_name)
+        browser.element("#lastName").set_value(second_name)
+        browser.element("#userEmail").set_value("fedotov@gmail.com")
+        browser.element("#genterWrapper").element(by.text("Other")).click()
+        browser.element("#userNumber").set_value("9999999999")
+        browser.element("#subjectsInput").send_keys("Maths")
+        browser.element("#subjectsInput").press_enter()
+        browser.element("#hobbiesWrapper").element(by.text("Sports")).click()
+        browser.element("#currentAddress").set_value("Some street 1")
+        browser.element("#state").click()
+        browser.element("#stateCity-wrapper").element(by.text("NCR")).click()
+        browser.element("#city").click()
+        browser.element("#stateCity-wrapper").element(by.text("Delhi")).click()
+        browser.element("#submit").click()
+
+    with allure.step("Check form results"):
+        browser.element("#example-modal-sizes-title-lg").should(have.text("Thanks for submitting the form"))
+
+    attachments.add_html(browser)
+    attachments.add_screenshot(browser)
+    attachments.add_logs(browser)
+    attachments.add_video(browser)
