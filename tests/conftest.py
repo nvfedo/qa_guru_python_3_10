43c0952da20e6574209130285741b8bf19f422b3
Nikita Fedotov <nik197.1997@gmail.com>
2023-03-28 03:49:27 +0300

    edit conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -26,9 +26,11 @@ def open_browser():
     }
 
     options.capabilities.update(selenoid_capabilities)
+    login = os.getenv('LOGIN')
+    password = os.getenv('PASSWORD')
 
     driver = webdriver.Remote(
-        command_executor="https://user1:1234@selenoid.autotests.cloud/wd/hub",
+        command_executor=f"https://{login}:{password}@selenoid.autotests.cloud/wd/hub",
         options=options
     )
     browser.config.driver = driver
